9d8b09b6cd413fcb04c15eceaa7efdf872779487
Jerald Wilks <jerald@wilks.pro>
2024-04-28 22:35:03 -0700

    Add button for testing.

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
 import Login from "./Login.jsx";
+import { Button } from "@mui/material";
 
 const ProtectedRoute = () => {
-  const { user, isAdmin, loading } = useAuth();
+  const { user, isAdmin, loading, logout } = useAuth();
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <>
+        <div>Loading...</div>
+        <Button onClick={logout()}></Button>
+      </>
+    );
   }
   if (!user || !isAdmin) {
     return <Login />;
